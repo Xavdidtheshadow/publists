d376bce66e9c8a1bacd5a5b495ac5b6ee2c0f3cb
David Brownman <beamneocube@gmail.com>
2016-05-26 18:19:46 -0700

    added encryption

--- config/db.js
+++ config/db.js
@@ -3,6 +3,27 @@
 const mongoose = require('mongoose')
 // i think this is global?
 mongoose.connect('mongodb://localhost/unparse')
+// Here's the required crypto code
+const crypto = require('crypto')
+const algo = 'aes-256-cbc'
+const encoding = 'utf-8'
+const format = 'hex'
+
+// encryption adapted from here: https://gist.github.com/kljensen/7505729
+function encrypt (text) {
+  var cipher = crypto.createCipher(algo, process.env.SERVER_SECRET)
+  var crypted = cipher.update(text, encoding, format)
+  crypted += cipher.final(format)
+  return crypted
+}
+
+function decrypt (text) {
+  if (text === null || typeof text === 'undefined') { return text }
+  var decipher = crypto.createDecipher(algo, process.env.SERVER_SECRET)
+  var dec = decipher.update(text, format, encoding)
+  dec += decipher.final(encoding)
+  return dec
+}
 
 let userSchema = mongoose.Schema({
   wid: {
@@ -11,18 +32,21 @@ let userSchema = mongoose.Schema({
     unique: true, // this is right from wlist, shouldn't be a problem
     index: true
   },
-  access_token: String,
+  // setter is used in findOneAndUpdate
+  access_token: {type: String, get: decrypt},
   name: {type: String, default: 'No Name'},
   public_lists: {type: mongoose.Schema.Types.Mixed, default: {}}
 })
+
+userSchema.set('toJSON', { getters: true })
 
 userSchema.statics.login = function (access_token, id, name) {
   return this.findOneAndUpdate({
     wid: id
   }, {
-    // should encrypt this
     $set: {
-      access_token: access_token,
+      // as long as whenever this is written we call encrypt, it's fine
+      access_token: encrypt(access_token),
       name: name
     }
   }, {
